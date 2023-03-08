d4252957be08a0ab62aafcb757fc76c1a91e437b
Mutesi <ktesialine@gmail.com>
2023-03-08 19:16:12 +0200

    fix:changing function name to lower case

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ const App = () => {
   }
   return (
     <div className="w-full h-screen flex justify-center items-center bg-blue-100">
-        <button className="text-xl" onClick={Message}>click me</button>
+        <button className="text-xl" onClick={message}>click me</button>
     </div>
   );
 }
-
 export default App
