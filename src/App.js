b4ef0d4ff2f82ac9c7b6f9cd0c0369048998930d
Mutesi <ktesialine@gmail.com>
2023-03-09 15:52:57 +0200

    fix:change function declaratin to arrow function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const App = () => {
  const[count, setCount]= useState(0)
- function startCount(){
+ const startCount = () =>{
  setCount((prevNumber) => prevNumber + 1)
  }
   return (
