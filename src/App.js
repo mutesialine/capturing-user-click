75ed87a54c8285b78251b7c40cc20ec915da6c82
Mutesi <ktesialine@gmail.com>
2023-03-09 17:44:42 +0200

    fix:change const variable from pascalcase to lower case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Data from "./Data/cardData.json"
+import data from "./data/cardData.json"
 import Card from "./components/ui/Card";
 
 const App = () => {
   return (
     <div className="w-full max-w-4xl flex mx-auto my-32 bg-green-100">
-      {Data.map((joke, index) => (
+      {data.map((joke, index) => (
         <Card {...joke} key={index} />
       ))}
     </div>
