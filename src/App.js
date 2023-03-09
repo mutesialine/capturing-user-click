bb835e932539f365dc0e92a3cf98da36a53715aa
Mutesi <ktesialine@gmail.com>
2023-03-09 15:45:18 +0200

    fix: changing text

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Card from "./components/ui/Card";
 const App = () => {
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-100">
-        { Data.map(())
-          
-        }<Card/>
+    <div className="w-full h-screen flex justify-center items-center bg-blue-100"> 
+        <Card/>
     </div>
   );
 }
