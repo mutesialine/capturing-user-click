ee89cf3a1cb5f895c1d10966738bd76e2830f161
Mutesi <ktesialine@gmail.com>
2023-03-09 17:39:41 +0200

    feat:card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
 const App = () => {
-  function message(e){
-    e.preventDefault();
-    alert("clicked!");
-  }
+
   return (
     <div className="w-full h-screen flex justify-center items-center bg-blue-100">
-        <button className="text-xl" onClick={message}>click me</button>
+        <Card/>
     </div>
   );
 }
