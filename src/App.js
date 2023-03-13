705404aa15a3269504dbaa64c18a1dd9f0022279
Mutesi <ktesialine@gmail.com>
2023-03-13 12:54:46 +0200

    feat:working with an api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import { useEffect, useState } from "react";
+import Card from "./components/ui/Card";
 const App = () => {
+  const [data, cardData]=useState([])
+  useEffect(()=>{
+    fetch("https://random-data-api.com/api/users/random_user?size=10")
+    .then((res) => res.json())
+      .then((data) => {
+        cardData(data);
+      });
+  },[])
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-100">
-        <Card/>
+    <div className="w-full h-full flex flex-col justify-center items-center bg-blue-100 py-32">
+      <button
+        className="px-4 py-2 rounded-lg bg-red-600 text-white text-bold"
+        onClick={cardData}
+      >
+        Fetch Random
+      </button>
+      <div className="grid grid-cols-2 gap-4 w-1/2 m-auto">
+        {data.map((element, index) => (
+          <Card {...element} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,11 +1,25 @@
 
-const Card = ({setup, punchline}) => {
+const Card = ({ first_name, last_name, avatar, employment }) => {
   return (
-    <div className="flex flex-col gap-y-6 bg-white m-8 p-8 shadow-md rounded-md">
-        <h2 className="text-2xl font-bold">{setup}</h2>
-        <p>{punchline}</p>
+    <div className="flex flex-col gap-y-6 bg-white m-8 p-8 shadow-md rounded-md cursor-pointer flip">
+      <div className="flip-content">
+        <div className="flip-back">
+          <p
+            className="text-xl font-bold transform-style-3d"
+          >
+            More InformationHere
+          </p>
+        </div>
+        <div className="flip-front">
+          <img src={avatar} alt="avatar" />
+          <h2 className="text-xl font-bold pt-3">
+            {first_name} {last_name}
+          </h2>
+          <p>{employment.title}</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Card
+export default Card;
